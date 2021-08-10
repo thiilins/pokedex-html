d02bac0c1f290398dbd0ab03c335d98473375b65
Thiago Lins <taglins@gmail.com>
2021-08-09 21:19:09 -0300

    Adicionado imagens e Corrigido Bugs no Script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,73 @@
-function iconType(typesArray) {
-  return typesArray.map((type) => {
-    return `
-          <a href="#">
-          <img class="card-type-icon ${type}-icon ${type}-icon-bg" title="${type}" alt="${type} icon" src="../assets/img/types/v1/${type}.svg">
-         </a> 
-          `;
-  });
-}
-
-function typesPokemon(pokemonObj) {
-  return pokemonObj.types.map((typeInfo) => typeInfo.type.name);
-}
-
-function idFormat(id) {
-  const idString = id.toString();
-  if (idString.length <= 1) {
-    return `00${id}`;
-  } else if (idString.length <= 2) {
-    return `0${id}`;
-  } else {
-    return idString;
-  }
-}
-
-function cardPattern(pokemonObj) {
-  const id = pokemonObj.id;
-  const name = pokemonObj.name;
-  const types = typesPokemon(pokemonObj);
-  const typeClass = types[0];
-  // ${typeClass}
-  return `<div id="${id}" class="card ${typeClass}-card-bg">
+const pattern = {
+  id: function (id) {
+    const idString = id.toString();
+    if (idString.length <= 1) {
+      return `00${id}`;
+    } else if (idString.length <= 2) {
+      return `0${id}`;
+    } else {
+      return idString;
+    }
+  },
+  icon: function (typesArray) {
+    return typesArray.map((type) => {
+      const pattern = `<a href="#">
+          <img class="card-type-icon ${type}-icon ${type}-icon-bg" title="${type}" alt="${type} icon" src="/assets/img/types/v1/${type}.svg">
+         </a>`;
+      return pattern;
+    });
+  },
+  card: function (pokemonObj) {
+    const id = pokemonObj.id;
+    const idCard = this.id(id);
+    const name = pokemonObj.name;
+    const types = typesPokemon(pokemonObj);
+    const icon = this.icon(types).join(" ");
+    const typeClass = types[0];
+    const pokemonImageFolder = "/assets/img/pokemons/";
+    const pattern = `<div id="${id}" class="card ${typeClass}-card-bg">
     <!--START CARDBOX  -->
 
       <div class="card-id-box">
-       <div class="card-id">#${idFormat(id)} | ${name}</div>
+       <div class="card-id">#${idCard} | ${name}</div>
       </div>
       <!--START IMAGE  -->
       <div class="card-image-box">
         <div class="card-image"
-          style="background-image: url('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg');">
+          style="background-image: url('${pokemonImageFolder}/${id}.png');">
         </div>
-        <div class="card-image-object-2 ${typeClass}-bg-color"
-          style="background-image: url('https://cdn.traction.one/pokedex/pokemon/${id}.png')"></div>
+    
       </div>
       <!--START TYPES  -->
       <div class="card-types-box">
         <div class="card-types">
-          ${iconType(types).join(" ")}
+          ${icon}
         </div>
 
     </div>
   </div>
   <!--END CARDBOX  -->
   </div>`;
-}
+    return pattern;
+  },
+};
 
+const typesPokemon = (pokemonObj) => {
+  return pokemonObj.types.map((typeInfo) => typeInfo.type.name);
+};
 const fetchPokemon = () => {
-  const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const urlAPI = "https://pokeapi.co/api/v2/pokemon";
+  const getPokemonUrl = (id) => `${urlAPI}/${id}`;
   const pokemonPromises = [];
-  for (let i = 1; i <= 150; i++) {
+  const TotalPokemonLoading = 151;
+  for (let i = 1; i <= TotalPokemonLoading; i++) {
     pokemonPromises.push(
       fetch(getPokemonUrl(i)).then((response) => response.json())
     );
   }
-
   Promise.all(pokemonPromises).then((pokemon) => {
     const lisPokemons = pokemon.reduce((acc, pokemon) => {
-      acc += cardPattern(pokemon);
+      acc += pattern.card(pokemon);
       return acc;
     }, "");
     const pokedex = document.querySelector('[data-js="pokedex');
