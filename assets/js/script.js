5080a98deef17df61a3efa4dce54c3c3e1c67294
Thiago Lins <taglins@gmail.com>
2021-08-16 00:56:47 -0300

    Comentando Código

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const TotalPokemonLoading = 151;
+//Formatando ID
 const idFormat = (id) => {
   const idString = id.toString();
   if (idString.length <= 1) {
@@ -9,6 +9,7 @@ const idFormat = (id) => {
     return idString;
   }
 };
+//Formatando Types Icon
 const elementIconFormat = (typesArray) => {
   return typesArray.map((type) => {
     const pattern = `<a href="#">
@@ -17,6 +18,14 @@ const elementIconFormat = (typesArray) => {
     return pattern;
   });
 };
+// Validando Origem da imagem (local ou remota)
+const imgCheck = (id) => {
+  const mainURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+  const alternativeURL = `/assets/img/pokemons/${id}.png`;
+  return id <= 649 ? mainURL : alternativeURL;
+};
+
+//Formatando Card
 const cardFormat = (pokemonObj) => {
   const id = pokemonObj.id;
   const idCard = idFormat(id);
@@ -48,35 +57,33 @@ const cardFormat = (pokemonObj) => {
   </div>`;
   return pattern;
 };
-
-const imgCheck = (id) => {
-  const mainURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
-  const alternativeURL = `/assets/img/pokemons/${id}.png`;
-  return id <= 649 ? mainURL : alternativeURL;
-};
-
+// Extraindo os tipos
 const typesPokemon = (pokemonObj) => {
   return pokemonObj.types.map((typeInfo) => typeInfo.type.name);
 };
+//Definindo URL para obtenção
 const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
 
+// Definindo número de pokemons carregados
+const TotalPokemonLoading = 151;
 const generatePokemonPromisses = () =>
   Array(TotalPokemonLoading)
     .fill()
     .map((_, index) =>
       fetch(getPokemonUrl(index + 1)).then((response) => response.json())
     );
-
+// Gerando o HTML
 const generateHTML = (pokemons) => {
   return pokemons.reduce((acc, pokemon) => {
     acc += cardFormat(pokemon);
     return acc;
   }, "");
 };
+//Inserindo os pokemons no arquivo HTML
 const insertPokemons = (pokemons) => {
   const pokedex = document.querySelector('[data-js="pokedex');
   pokedex.innerHTML = pokemons;
 };
-
+// Promisses
 const pokemonPromises = generatePokemonPromisses();
 Promise.all(pokemonPromises).then(generateHTML).then(insertPokemons);
